9a796183385738f0b38fca0b6dcfec039528711c
HugoLorent <77060035+HugoLorent@users.noreply.github.com>
2022-10-17 10:41:03 +0200

    finish question 1
    Signed-off-by: HugoLorent <77060035+HugoLorent@users.noreply.github.com>

---
+++ Character.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace event_timers_project
+{
+    class Character
+    {
+        public string Name { get; set; }
+        public List<Character> Friends { get; set; }
+        public List<Character> Ennemies { get; set; }
+        public int LifeTime { get; set; }
+
+        public Character(string name, int lifeTime)
+        {
+            this.Name = name;
+            this.LifeTime = lifeTime;
+            this.Friends = new List<Character>();
+            this.Ennemies = new List<Character>();
+        }
+
+        public Character(string name)
+        {
+            Random rand = new Random();
+            this.Name = name;
+            this.LifeTime = rand.Next(1, 10);
+            this.Friends = new List<Character>();
+            this.Ennemies = new List<Character>();
+        }
+
+        public void MakingFriendsAndEnnemies(List<Character> characters)
+        {
+            foreach (Character character in characters)
+            {
+
+                if (character != this)
+                {
+                    FriendOrEnnemy(character);
+                }
+            }
+        }
+
+        private void FriendOrEnnemy(Character character)
+        {
+            Random rand = new Random();
+            int friendOrEnnemy = rand.Next(0, 2);
+
+            if (friendOrEnnemy == 0)
+            {
+                if (!character.Friends.Contains(this))
+                {
+                    this.Ennemies.Add(character);
+                    Console.WriteLine($"{this.Name} considère {character.Name} comme un ennemi");
+                }
+            }
+            else
+            {
+                if (!character.Ennemies.Contains(this))
+                {
+                    this.Friends.Add(character);
+                    Console.WriteLine($"{this.Name} considère {character.Name} comme un ami");
+                }
+            }
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,12 +1,40 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace event_timers_project
 {
     class Program
     {
+        public static List<Character> characters = new List<Character>();
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Character remy = new Character("Rémy");
+            Character olivier = new Character("Olivier");
+            Character hugo = new Character("Hugo");
+            characters.Add(remy);
+            characters.Add(olivier);
+            characters.Add(hugo);
+
+            foreach (Character character in characters)
+            {
+                character.MakingFriendsAndEnnemies(characters);
+                Console.WriteLine();
+            }
+
+            foreach (Character character in characters)
+            {
+                foreach (Character friend in character.Friends)
+                {
+                    Console.WriteLine($"{character.Name} est ami avec {friend.Name}");
+                }
+
+                foreach (Character ennemy in character.Ennemies)
+                {
+                    Console.WriteLine($"{character.Name} est ennemi avec {ennemy.Name}");
+                }
+
+                Console.WriteLine();
+            }
         }
     }
 }
